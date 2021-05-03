d2c23dc7768af1bac4ded17ea540c14cb2a3aa99
Matthew <elijah2895@hotmail.com>
2021-05-03 17:10:13 -0400

    Finished for now

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushis, setSushis] = useState([])
+  const [wallet, setWallet] = useState(100)
+
+  useEffect(() => {
+    fetch(API)
+    .then(r => r.json())
+    .then(sushis => {
+      const updatedSushis = sushis.map(sushi => {
+        if (sushi.id === eatenSushi.id) return {...sushi, eaten: true};
+        return sushi;
+      })
+    })
+  })
+
+  const eatenSushi = sushis.filter( sushi => sushi.eaten)
+  
   return (
     <div className="app">
-      <SushiContainer />
+      <SushiContainer sushis={sushis} />
       <Table />
     </div>
   );
